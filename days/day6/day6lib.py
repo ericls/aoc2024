7998b5bdacaa61ee0860418fb623ed35a882a030
Shen Li <dustet@gmail.com>
2024-12-06 11:22:33 -0500

    don't need to rebuild index for every grid

--- days/day6/day6lib.py
+++ days/day6/day6lib.py
@@ -2,7 +2,6 @@ from bisect import bisect, insort
 from collections import defaultdict
 from dataclasses import dataclass
 from enum import Enum, auto
-from functools import cached_property
 
 from lib.input import get_input_lines
 from lib.path import Pt
@@ -20,51 +19,62 @@ class Grid:
     width: int
     height: int
     bricks: set[Pt]
+    bricks_index: tuple[dict[int, list[int]], dict[int, list[int]]]
 
     def place_brick(self, pos):
-        return Grid(self.width, self.height, set(self.bricks) | {pos})
-
-    @cached_property
-    def indexed_bricks(self):
-        by_x: dict[int, list[int]] = defaultdict(list)
-        by_y: dict[int, list[int]] = defaultdict(list)
-        for brick in self.bricks:
-            insort(by_x[brick.x], brick.y)
-            insort(by_y[brick.y], brick.x)
-        return by_x, by_y
-
-    def next_brick(self, pos: Pt, direction: Pt):
-        by_x, by_y = self.indexed_bricks
-
-        if direction == UP:
-            x = pos.x
-            index = bisect(by_x[x], pos.y)
-            if index > 0:
-                return Pt(x, by_x[x][index - 1])
-            return None
-        if direction == DOWN:
-            x = pos.x
-            index = bisect(by_x[x], pos.y)
-            if index < len(by_x[x]):
-                return Pt(x, by_x[x][index])
-            return None
-        if direction == LEFT:
-            y = pos.y
-            index = bisect(by_y[y], pos.x)
-            if index > 0:
-                return Pt(by_y[y][index - 1], y)
-            return None
-        if direction == RIGHT:
-            y = pos.y
-            index = bisect(by_y[y], pos.x)
-            if index < len(by_y[y]):
-                return Pt(by_y[y][index], y)
-            return None
+        by_x, by_y = self.bricks_index
+        new_by_x = defaultdict(list)
+        new_by_y = defaultdict(list)
+        new_by_x.update({k: [*v] for k, v in by_x.items()})
+        new_by_y.update({k: [*v] for k, v in by_y.items()})
+        insort(new_by_x[pos.x], pos.y)
+        insort(new_by_y[pos.y], pos.x)
+        return Grid(
+            self.width, self.height, set(self.bricks) | {pos}, (new_by_x, new_by_y)
+        )
 
 
 class Action(Enum):
     VISIT = auto()
     TURN = auto()
+
+
+def build_bricks_index(bricks):
+    by_x: dict[int, list[int]] = defaultdict(list)
+    by_y: dict[int, list[int]] = defaultdict(list)
+    for brick in bricks:
+        insort(by_x[brick.x], brick.y)
+        insort(by_y[brick.y], brick.x)
+    return by_x, by_y
+
+
+def next_brick(bricks_index, pos: Pt, direction: Pt):
+    by_x, by_y = bricks_index
+
+    if direction == UP:
+        x = pos.x
+        index = bisect(by_x[x], pos.y)
+        if index > 0:
+            return Pt(x, by_x[x][index - 1])
+        return None
+    if direction == DOWN:
+        x = pos.x
+        index = bisect(by_x[x], pos.y)
+        if index < len(by_x[x]):
+            return Pt(x, by_x[x][index])
+        return None
+    if direction == LEFT:
+        y = pos.y
+        index = bisect(by_y[y], pos.x)
+        if index > 0:
+            return Pt(by_y[y][index - 1], y)
+        return None
+    if direction == RIGHT:
+        y = pos.y
+        index = bisect(by_y[y], pos.x)
+        if index < len(by_y[y]):
+            return Pt(by_y[y][index], y)
+        return None
 
 
 def get_grid_and_start():
@@ -81,7 +91,7 @@ def get_grid_and_start():
         for x, cell in enumerate(row):
             if cell == "#":
                 bricks.add(Pt(x, y))
-    return Grid(width, height, bricks), start_pos
+    return Grid(width, height, bricks, build_bricks_index(bricks)), start_pos
 
 
 def in_bound(grid, pt):
@@ -118,7 +128,7 @@ def is_loop(grid: Grid, starting_pos, starting_direction):
     turns_set = set()
 
     while True:
-        next_turn_pos = grid.next_brick(current_pos, current_direction)
+        next_turn_pos = next_brick(grid.bricks_index, current_pos, current_direction)
         if next_turn_pos is None:
             return False
         current_pos = next_turn_pos - current_direction
